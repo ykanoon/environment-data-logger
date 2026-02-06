0b8a568f1b3e114043af0c2464f9cec8e0ad1a78
YoKanno0504 <yocean@mac.com>
2026-02-06 16:42:37 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,43 @@
+datalogger.onLogFull(function () {
+    logging = false
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    logging = true
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Skull)
+    datalogger.deleteLog()
+    datalogger.setColumnTitles(
+    "temperature",
+    "light"
+    )
+})
+input.onButtonPressed(Button.B, function () {
+    logging = false
+    basic.showIcon(IconNames.No)
+})
+let logging = false
+logging = false
+basic.showIcon(IconNames.No)
+datalogger.setColumnTitles(
+"temperature",
+"light"
+)
+loops.everyInterval(3600000, function () {
+    if (logging) {
+        basic.showIcon(IconNames.Heart)
+        datalogger.log(
+        datalogger.createCV("temperature", input.temperature()),
+        datalogger.createCV("light", input.lightLevel())
+        )
+        basic.clearScreen()
+    }
+})
