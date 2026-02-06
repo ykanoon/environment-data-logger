80ffb67cad3683cade7180d0140ecf66ed2b21d1
YoKanno0504 <yocean@mac.com>
2026-02-06 16:52:49 +0900

    表示を消して節約

--- main.ts
+++ main.ts
@@ -11,12 +11,15 @@ datalogger.onLogFull(function () {
 input.onButtonPressed(Button.A, function () {
     logging = true
     basic.showIcon(IconNames.Yes)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
     if (logging) {
         basic.showIcon(IconNames.Diamond)
+        basic.clearScreen()
     } else {
         basic.showIcon(IconNames.No)
+        basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.AB, function () {
@@ -26,18 +29,21 @@ input.onButtonPressed(Button.AB, function () {
     "temperature",
     "light"
     )
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     logging = false
     basic.showIcon(IconNames.No)
+    basic.clearScreen()
 })
 let logging = false
-logging = false
-basic.showIcon(IconNames.No)
+logging = true
+basic.showIcon(IconNames.Yes)
 datalogger.setColumnTitles(
 "temperature",
 "light"
 )
+basic.clearScreen()
 loops.everyInterval(3600000, function () {
     if (logging) {
         basic.showIcon(IconNames.Heart)
