de78f44321890e63c80fbc3ddfb80726e8d7e5fe
YoKanno0504 <yocean@mac.com>
2026-02-07 14:27:45 +0900

    湿度追加

--- main.ts
+++ main.ts
@@ -14,7 +14,6 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
-    logging = true
     if (logging) {
         basic.showIcon(IconNames.Diamond)
         basic.clearScreen()
@@ -27,8 +26,10 @@ input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Skull)
     datalogger.deleteLog()
     datalogger.setColumnTitles(
-    "temperature",
-    "light"
+    "temp_CPU",
+    "light",
+    "temp_OUT",
+    "moist_lv"
     )
     basic.clearScreen()
 })
@@ -41,25 +42,19 @@ let logging = false
 logging = true
 basic.showIcon(IconNames.Yes)
 datalogger.setColumnTitles(
-"temperature",
-"light"
+"temp_CPU",
+"light",
+"temp_OUT",
+"moist_lv"
 )
 datalogger.includeTimestamp(FlashLogTimeStampFormat.Seconds)
 basic.clearScreen()
 loops.everyInterval(1000, function () {
-    logging = true
     datalogger.log(
-    datalogger.createCV("temperature", input.temperature()),
+    datalogger.createCV("temp_CPU", input.temperature()),
     datalogger.createCV("light", input.lightLevel()),
-    datalogger.createCV("temp2", pins.digitalReadPin(DigitalPin.P0)),
-    datalogger.createCV("year", DS3231.year()),
-    datalogger.createCV("month", DS3231.month()),
-    datalogger.createCV("date", DS3231.date()),
-    datalogger.createCV("hour", DS3231.hour()),
-    datalogger.createCV("min", DS3231.minute()),
-    datalogger.createCV("sec", DS3231.second())
+    datalogger.createCV("temp_OUT", stem.TP2_getTemperature()),
+    datalogger.createCV("moist_lv", pins.analogReadPin(AnalogReadWritePin.P1))
     )
-    if (logging) {
-    	
-    }
+    logging = true
 })
